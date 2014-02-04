f5fdfd6ed874720b497ff1abd2808512ca4ef9ed
Ryan Nowak <nowakra@gmail.com>
2014-02-03 18:26:53 -0800

    Mock of magical service discovery

---
+++ src/Microsoft.AspNet.DependencyInjection/DefaultServiceAttribute.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace Microsoft.AspNet.DependencyInjection
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class DefaultServiceAttribute : Attribute
+    {
+        public DefaultServiceAttribute(Type serviceType, Type implementationType)
+        {
+            ServiceType = serviceType;
+            ImplementationType = implementationType;
+        }
+
+        public Type ImplementationType { get; private set; }
+
+        public Type ServiceType { get; private set; }
+    }
+}
---
+++ src/Microsoft.AspNet.DependencyInjection/DefaultServiceDiscovery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Microsoft.AspNet.DependencyInjection
+{
+    internal static class DefaultServiceDiscovery
+    {
+        public static void AddServices(ServiceProvider services)
+        {
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (var attribute in assembly.GetCustomAttributes(typeof(DefaultServiceAttribute), inherit: false).Cast<DefaultServiceAttribute>())
+                {
+                    services.Add(attribute.ServiceType, attribute.ImplementationType);
+                }
+            }
+        }
+    }
+}
