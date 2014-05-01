286c6fec57926b9885b9df24ed37258b29bf9924
Hao Kung <haok@microsoft.com>
2014-05-01 11:29:38 -0700

    Add and use NotNull

--- src/Microsoft.AspNet.DependencyInjection/ServiceCollectionExtensions.cs
+++ src/Microsoft.AspNet.DependencyInjection/ServiceCollectionExtensions.cs
@@ -7,103 +7,58 @@ namespace Microsoft.AspNet.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddTransient<TService, TImplementation>(this IServiceCollection services)
+        public static IServiceCollection AddTransient<TService, TImplementation>([NotNull]this IServiceCollection services)
         {
-            if (services == null)
-            {
-                throw new ArgumentNullException("services");
-            }
             return services.AddTransient(typeof(TService), typeof(TImplementation));
         }
 
-        public static IServiceCollection AddTransient(this IServiceCollection services, Type serviceType)
+        public static IServiceCollection AddTransient([NotNull]this IServiceCollection services, Type serviceType)
         {
-            if (services == null)
-            {
-                throw new ArgumentNullException("services");
-            }
             return services.AddTransient(serviceType, serviceType);
         }
 
-        public static IServiceCollection AddTransient<TService>(this IServiceCollection services)
+        public static IServiceCollection AddTransient<TService>([NotNull]this IServiceCollection services)
         {
-            if (services == null)
-            {
-                throw new ArgumentNullException("services");
-            }
             return services.AddTransient(typeof(TService));
         }
 
-        public static IServiceCollection AddScoped<TService, TImplementation>(this IServiceCollection services)
+        public static IServiceCollection AddScoped<TService, TImplementation>([NotNull]this IServiceCollection services)
         {
-            if (services == null)
-            {
-                throw new ArgumentNullException("services");
-            }
             return services.AddScoped(typeof(TService), typeof(TImplementation));
         }
 
-        public static IServiceCollection AddScoped(this IServiceCollection services, Type serviceType)
+        public static IServiceCollection AddScoped([NotNull]this IServiceCollection services, Type serviceType)
         {
-            if (services == null)
-            {
-                throw new ArgumentNullException("services");
-            }
             return services.AddScoped(serviceType, serviceType);
         }
 
-        public static IServiceCollection AddScoped<TService>(this IServiceCollection services)
+        public static IServiceCollection AddScoped<TService>([NotNull]this IServiceCollection services)
         {
-            if (services == null)
-            {
-                throw new ArgumentNullException("services");
-            }
             return services.AddScoped(typeof(TService));
         }
 
-        public static IServiceCollection AddSingleton<TService, TImplementation>(this IServiceCollection services)
+        public static IServiceCollection AddSingleton<TService, TImplementation>([NotNull]this IServiceCollection services)
         {
-            if (services == null)
-            {
-                throw new ArgumentNullException("services");
-            }
             return services.AddSingleton(typeof(TService), typeof(TImplementation));
         }
 
-        public static IServiceCollection AddSingleton(this IServiceCollection services, Type serviceType)
+        public static IServiceCollection AddSingleton([NotNull]this IServiceCollection services, Type serviceType)
         {
-            if (services == null)
-            {
-                throw new ArgumentNullException("services");
-            }
             return services.AddSingleton(serviceType, serviceType);
         }
 
-        public static IServiceCollection AddSingleton<TService>(this IServiceCollection services)
+        public static IServiceCollection AddSingleton<TService>([NotNull]this IServiceCollection services)
         {
-            if (services == null)
-            {
-                throw new ArgumentNullException("services");
-            }
             return services.AddSingleton(typeof(TService));
         }
 
-        public static IServiceCollection AddInstance<TService>(this IServiceCollection services, TService implementationInstance)
+        public static IServiceCollection AddInstance<TService>([NotNull]this IServiceCollection services, TService implementationInstance)
         {
-            if (services == null)
-            {
-                throw new ArgumentNullException("services");
-            }
             return services.AddInstance(typeof(TService), implementationInstance);
         }
 
-        public static IServiceCollection AddSetup(this IServiceCollection services, Type setupType)
+        public static IServiceCollection AddSetup([NotNull]this IServiceCollection services, Type setupType)
         {
-            if (services == null)
-            {
-                throw new ArgumentNullException("services");
-            }
-
             var serviceTypes = setupType.GetInterfaces()
                 .Where(t => t.GetTypeInfo().IsGenericType && t.GetGenericTypeDefinition() == typeof (IOptionsSetup<>));
             foreach (var serviceType in serviceTypes)
@@ -118,27 +73,13 @@ namespace Microsoft.AspNet.DependencyInjection
             return services;
         }
 
-        public static IServiceCollection AddSetup<TSetup>(this IServiceCollection services)
+        public static IServiceCollection AddSetup<TSetup>([NotNull]this IServiceCollection services)
         {
-            if (services == null)
-            {
-                throw new ArgumentNullException("services");
-            }
-
             return services.AddSetup(typeof (TSetup));
         }
 
-        public static IServiceCollection AddSetup(this IServiceCollection services, object setupInstance)
+        public static IServiceCollection AddSetup([NotNull]this IServiceCollection services, [NotNull]object setupInstance)
         {
-            if (services == null)
-            {
-                throw new ArgumentNullException("services");
-            }
-            if (setupInstance == null)
-            {
-                throw new ArgumentNullException("setupInstance");
-            }
-
             var setupType = setupInstance.GetType();
             var serviceTypes = setupType.GetInterfaces()
                 .Where(t => t.GetTypeInfo().IsGenericType && t.GetGenericTypeDefinition() == typeof (IOptionsSetup<>));
@@ -155,25 +96,17 @@ namespace Microsoft.AspNet.DependencyInjection
         }
 
 
-        public static IServiceCollection SetupOptions<TOptions>(this IServiceCollection services,
+        public static IServiceCollection SetupOptions<TOptions>([NotNull]this IServiceCollection services,
             Action<TOptions> setupAction,
             int order)
         {
-            if (services == null)
-            {
-                throw new ArgumentNullException("services");
-            }
             services.AddSetup(new OptionsSetup<TOptions>(setupAction) {Order = order});
             return services;
         }
 
-        public static IServiceCollection SetupOptions<TOptions>(this IServiceCollection services,
+        public static IServiceCollection SetupOptions<TOptions>([NotNull]this IServiceCollection services,
             Action<TOptions> setupAction)
         {
-            if (services == null)
-            {
-                throw new ArgumentNullException("services");
-            }
             return services.SetupOptions<TOptions>(setupAction, order: 0);
         }
     }
