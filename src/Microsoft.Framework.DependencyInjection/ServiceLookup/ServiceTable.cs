46399d3f5af42045d765c913a5bfb22e72629af5
Hao Kung <haok@microsoft.com>
2014-11-14 18:47:08 -0800

    Remove using

--- src/Microsoft.Framework.DependencyInjection/ServiceLookup/ServiceTable.cs
+++ src/Microsoft.Framework.DependencyInjection/ServiceLookup/ServiceTable.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 
 namespace Microsoft.Framework.DependencyInjection.ServiceLookup
