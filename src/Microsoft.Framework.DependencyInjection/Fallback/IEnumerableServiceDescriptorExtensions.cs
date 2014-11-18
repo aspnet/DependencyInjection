0b4f2a15faa216ddb2fad39cab4b662632378ea1
Hao Kung <haok@microsoft.com>
2014-11-18 00:59:06 -0800

    Rename extension file

---
+++ src/Microsoft.Framework.DependencyInjection/Fallback/IEnumerableServiceDescriptorExtensions.cs
@@ -0,0 +1,44 @@
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Framework.DependencyInjection.ServiceLookup;
+
+namespace Microsoft.Framework.DependencyInjection.Fallback
+{
+    public static class IEnumerableServiceDescriptorExtensions
+    {
+        public static IServiceProvider BuildServiceProvider(this IEnumerable<IServiceDescriptor> services)
+        {
+            return new ServiceProvider(services);
+        }
+
+        public static IServiceProvider BuildFallbackServiceProvider(this IEnumerable<IServiceDescriptor> services)
+        {
+            // Build the manifest
+            var manifestTypes = services.Where(t => t.ServiceType.GenericTypeArguments.Length == 0 
+                    && t.ServiceType.GetTypeInfo().GenericTypeParameters.Length == 0
+                    && t.ServiceType != typeof(IServiceManifest))
+                    .Select(t => t.ServiceType).Distinct();
+            return new ServiceProvider(services.Concat(new IServiceDescriptor[]
+            {
+                new ServiceDescriber()
+                    .Instance<IServiceManifest>(
+                        new ServiceManifest(manifestTypes))
+            }));
+        }
+
+        private class ServiceManifest : IServiceManifest
+        {
+            public ServiceManifest(IEnumerable<Type> services)
+            {
+                Services = services;
+            }
+
+            public IEnumerable<Type> Services { get; private set; }
+        }
+    }
+}
