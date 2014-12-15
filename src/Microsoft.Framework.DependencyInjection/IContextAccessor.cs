d0639261d9df0255c94c334cba666a09eda9bac8
Hao Kung <haok@microsoft.com>
2014-12-15 15:24:26 -0800

    Remove SetSource from ContextAccessor

--- src/Microsoft.Framework.DependencyInjection/IContextAccessor.cs
+++ src/Microsoft.Framework.DependencyInjection/IContextAccessor.cs
@@ -10,7 +10,5 @@ namespace Microsoft.Framework.DependencyInjection
         TContext Value { get; }
 
         TContext SetValue(TContext value);
-
-        IDisposable SetContextSource(Func<TContext> access, Func<TContext, TContext> exchange);
     }
 }
