3ce74ff6e5aaf5ad07495421c1226bb462a7fa87
Kev Hunter <mrkevhunter@gmail.com>
2015-05-05 23:21:03 +0100

    added structuremap support

--- test/Microsoft.Framework.DependencyInjection.Tests/StructureMapContainerTests.cs
+++ test/Microsoft.Framework.DependencyInjection.Tests/StructureMapContainerTests.cs
@@ -3,63 +3,18 @@
 
 #if !DNXCORE50
 using System;
+using Microsoft.Framework.DependencyInjection.Structuremap;
 using Microsoft.Framework.DependencyInjection.Tests.Fakes;
 using StructureMap;
 
 namespace Microsoft.Framework.DependencyInjection.Tests
 {
-    public class StructureMapContainerTests : AllContainerTestsBase
+    public class StructureMapContainerTests : ScopingContainerTestBase
     {
         protected override IServiceProvider CreateContainer()
         {
-            var container = new Container(builder =>
-            {
-                foreach (var descriptor in TestServices.DefaultServices())
-                {
-                    if (descriptor.ImplementationType != null)
-                    {
-                        builder.For(descriptor.ServiceType).Use(descriptor.ImplementationType);
-                    }
-                    else if (descriptor.ImplementationFactory != null)
-                    {
-                        builder.For(descriptor.ServiceType)
-                               .Use(context =>
-                               {
-                                   var provider = context.GetInstance<IServiceProvider>();
-                                   return descriptor.ImplementationFactory(provider);
-                               });
-                    }
-                    else
-                    {
-                        builder.For(descriptor.ServiceType).Use(descriptor.ImplementationInstance);
-                    }
-                }
-
-                builder.For<IServiceProvider>().Use<StructureMapServiceProvider>();
-            });
-
+            var container = StructuremapRegistration.Populate(TestServices.DefaultServices());
             return container.GetInstance<IServiceProvider>();
-        }
-
-        private class StructureMapServiceProvider : IServiceProvider
-        {
-            private readonly IContainer _container;
-
-            public StructureMapServiceProvider(IContainer container)
-            {
-                _container = container;
-            }
-
-            public object GetService(Type type)
-            {
-                return _container.TryGetInstance(type) ?? GetMultiService(type);
-            }
-
-            private object GetMultiService(Type collectionType)
-            {
-                return MultiServiceHelpers.GetMultiService(collectionType,
-                    serviceType => _container.GetAllInstances(serviceType));
-            }
         }
     }
 }
