211a0bb5a13f1e668f9a188ef06bcc7df1ee6828
Eilon Lipton <elipton@microsoft.com>
2015-11-05 16:41:57 -0800

    Add doc comments for mainline APIs
    #317

--- src/Microsoft.Extensions.DependencyInjection.Abstractions/ServiceLifetime.cs
+++ src/Microsoft.Extensions.DependencyInjection.Abstractions/ServiceLifetime.cs
@@ -3,10 +3,22 @@
 
 namespace Microsoft.Extensions.DependencyInjection
 {
+    /// <summary>
+    /// Specifies the lifetime of a service in an <see cref="IServiceCollection"/>.
+    /// </summary>
     public enum ServiceLifetime
     {
+        /// <summary>
+        /// Specifies that a single instance of the service will be created.
+        /// </summary>
         Singleton,
+        /// <summary>
+        /// Specifies that a new instance of the service will be created for each scope.
+        /// </summary>
         Scoped,
+        /// <summary>
+        /// Specifies that a new instance of the service will be created every time it is requested.
+        /// </summary>
         Transient
     }
 }
