0f10ff090a18effcaf6d8b9fdd0356dbdf805f46
Pavel Krymets <pavel@krymets.com>
2016-07-13 12:12:55 -0700

    Add call site type name to unsupported message

--- src/Microsoft.Extensions.DependencyInjection/ServiceLookup/CallSiteVisitor.cs
+++ src/Microsoft.Extensions.DependencyInjection/ServiceLookup/CallSiteVisitor.cs
@@ -66,7 +66,7 @@ namespace Microsoft.Extensions.DependencyInjection.ServiceLookup
             {
                 return VisitServiceScopeService(serviceScopeService, argument);
             }
-            throw new NotSupportedException("Call site type is not supported");
+            throw new NotSupportedException($"Call site type {callSite.GetType()} is not supported");
         }
 
         protected abstract TResult VisitTransient(TransientCallSite transientCallSite, TArgument argument);
