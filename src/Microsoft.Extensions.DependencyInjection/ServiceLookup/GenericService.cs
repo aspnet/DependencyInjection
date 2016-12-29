37e66d9ccc872640ce4b2dfccf1813665781d6f0
Jimmy Bogard <jimmy.bogard@gmail.com>
2016-12-29 15:54:06 -0600

    Returning a null service when the closed implementation type cannot be successfully created based on the generic arguments (most likely because the generic arguments do not adhere to the open generic type's constraints). However, the ArgumentException can be thrown for a couple of other cases which could be tested individually.

--- src/Microsoft.Extensions.DependencyInjection/ServiceLookup/GenericService.cs
+++ src/Microsoft.Extensions.DependencyInjection/ServiceLookup/GenericService.cs
@@ -23,8 +23,16 @@ namespace Microsoft.Extensions.DependencyInjection.ServiceLookup
         public IService GetService(Type closedServiceType)
         {
             Type[] genericArguments = closedServiceType.GetTypeInfo().GenericTypeArguments;
-            Type closedImplementationType =
-                _descriptor.ImplementationType.MakeGenericType(genericArguments);
+            Type closedImplementationType;
+            
+            try 
+            {
+                closedImplementationType = _descriptor.ImplementationType.MakeGenericType(genericArguments);
+            }
+            catch (ArgumentException)
+            {
+                return null;
+            }
 
             var closedServiceDescriptor = new ServiceDescriptor(closedServiceType, closedImplementationType, Lifetime);
             return new Service(closedServiceDescriptor);
