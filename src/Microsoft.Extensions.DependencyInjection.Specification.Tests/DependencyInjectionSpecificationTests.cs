6454f24a3e4bf00f68ee29104caa22d00e74d858
Pavel Krymets <pavel@krymets.com>
2017-06-01 15:28:41 -0700

    Modify test

--- src/Microsoft.Extensions.DependencyInjection.Specification.Tests/DependencyInjectionSpecificationTests.cs
+++ src/Microsoft.Extensions.DependencyInjection.Specification.Tests/DependencyInjectionSpecificationTests.cs
@@ -686,23 +686,25 @@ namespace Microsoft.Extensions.DependencyInjection.Specification
         {
             // Arrange
             var serviceCollection = new TestServiceCollection();
-            var instance = new FakeOpenGenericService<IFakeService>(null);
+            var instance = new FakeOpenGenericService<PocoClass>(null);
 
-            serviceCollection.AddTransient<IFakeService, FakeService>();
+            serviceCollection.AddTransient<PocoClass, PocoClass>();
+            serviceCollection.AddSingleton(typeof(IFakeOpenGenericService<PocoClass>), typeof(FakeService));
             serviceCollection.AddSingleton(typeof(IFakeOpenGenericService<>), typeof(FakeOpenGenericService<>));
-            serviceCollection.AddSingleton<IFakeOpenGenericService<IFakeService>>(instance);
-            serviceCollection.AddSingleton(typeof(IFakeOpenGenericService<IFakeService>), typeof(FakeOpenGenericService<IFakeService>));
+            serviceCollection.AddSingleton<IFakeOpenGenericService<PocoClass>>(instance);
 
             var serviceProvider = CreateServiceProvider(serviceCollection);
 
-            var enumerable = serviceProvider.GetService<IEnumerable<IFakeOpenGenericService<IFakeService>>>().ToArray();
+            var enumerable = serviceProvider.GetService<IEnumerable<IFakeOpenGenericService<PocoClass>>>().ToArray();
 
             // Assert
             Assert.Equal(3, enumerable.Length);
             Assert.NotNull(enumerable[0]);
             Assert.NotNull(enumerable[1]);
             Assert.NotNull(enumerable[2]);
-            Assert.Equal(instance, enumerable[1]);
+
+            Assert.Equal(instance, enumerable[2]);
+            Assert.IsType<FakeService>(enumerable[0]);
         }
     }
 }
