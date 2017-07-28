b765b91933b56745484dc90b9ad242675cf3bcd2
Pavel Krymets <pavel@krymets.com>
2017-07-28 12:21:35 -0700

    ObjectFactory.cs

--- shared/Microsoft.Extensions.ActivatorUtilities.Sources/ActivatorUtilities.cs
+++ shared/Microsoft.Extensions.ActivatorUtilities.Sources/ActivatorUtilities.cs
@@ -16,19 +16,6 @@ namespace Microsoft.Extensions.Internal
 #endif
 {
     /// <summary>
-    /// The result of <see cref="ActivatorUtilities.CreateFactory(Type, Type[])"/>.
-    /// </summary>
-    /// <param name="serviceProvider">The <see cref="IServiceProvider"/> to get service arguments from.</param>
-    /// <param name="arguments">Additional constructor arguments.</param>
-    /// <returns>The instantiated type.</returns>
-#if ActivatorUtilities_In_DependencyInjection
-    public
-#else
-    internal
-#endif
-    delegate object ObjectFactory(IServiceProvider serviceProvider, object[] arguments);
-
-    /// <summary>
     /// Helper code for the various activator services.
     /// </summary>
 
