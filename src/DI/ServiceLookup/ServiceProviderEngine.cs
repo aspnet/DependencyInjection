f6ced28f11c152a2bb5870402ffe8906b4013601
Pavel Krymets <pavel@krymets.com>
2017-08-03 10:19:46 -0700

    (C) and move files around

--- src/DI/ServiceLookup/ServiceProviderEngine.cs
+++ src/DI/ServiceLookup/ServiceProviderEngine.cs
@@ -1,11 +1,13 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection.ServiceLookup;
 
-namespace Microsoft.Extensions.DependencyInjection
+namespace Microsoft.Extensions.DependencyInjection.ServiceLookup
 {
     internal class ServiceProviderEngine : IServiceProviderEngine, IServiceScopeFactory
     {
--- src/DI/ServiceLookup/ServiceProviderEngineScope.cs
+++ src/DI/ServiceLookup/ServiceProviderEngineScope.cs
@@ -1,7 +1,10 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using System;
 using System.Collections.Generic;
 
-namespace Microsoft.Extensions.DependencyInjection
+namespace Microsoft.Extensions.DependencyInjection.ServiceLookup
 {
     internal class ServiceProviderEngineScope : IServiceScope, IServiceProvider
     {
