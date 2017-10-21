a8a3c59770426fddfe569db6660d7a450aa01bdc
andrey.tretyak <andrey.tretyak@altium.com>
2017-10-21 17:21:58 +0300

    Unit test fixes

--- test/DI.Tests/ServiceTableTest.cs
+++ test/DI.Tests/ServiceTableTest.cs
@@ -26,7 +26,7 @@ namespace Microsoft.Extensions.DependencyInjection.ServiceLookup
             ExceptionAssert.ThrowsArgument(
                 () => new CallSiteFactory(serviceDescriptors),
                 "descriptors",
-                $"Open generic service type '{typeof(IList<>)}' requires registering an open generic implementation type.");
+                "Open generic service type 'System.Collections.Generic.IList' requires registering an open generic implementation type.");
         }
 
         public static TheoryData Constructor_WithInstance_ThrowsIfServiceTypeIsOpenGenericData =>
@@ -51,7 +51,7 @@ namespace Microsoft.Extensions.DependencyInjection.ServiceLookup
             var ex = ExceptionAssert.ThrowsArgument(
                 () => new CallSiteFactory(serviceDescriptors),
                 "descriptors",
-                $"Open generic service type '{typeof(IEnumerable<>)}' requires registering an open generic implementation type.");
+                "Open generic service type 'System.Collections.Generic.IEnumerable' requires registering an open generic implementation type.");
         }
 
         [Fact]
@@ -67,7 +67,7 @@ namespace Microsoft.Extensions.DependencyInjection.ServiceLookup
             var ex = ExceptionAssert.ThrowsArgument(
                 () => new CallSiteFactory(serviceDescriptors),
                 "descriptors",
-                $"Open generic service type '{typeof(Tuple<>)}' requires registering an open generic implementation type.");
+                "Open generic service type 'System.Tuple' requires registering an open generic implementation type.");
         }
     }
 }
